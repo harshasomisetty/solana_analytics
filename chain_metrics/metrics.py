10d8818bc5f9e68034ebf82e9f114cbfa5183105
Harsha Somisetty <harshasomisetty7@gmail.com>
2022-05-23 12:20:01 -0500

    shitty pandas

--- chain_metrics/metrics.py
+++ chain_metrics/metrics.py
@@ -1,54 +1,26 @@
-import json
 import pandas as pd
-import matplotlib.pyplot as plt
-import os
-from natsort import natsorted
 
-connection_string = "devnet2/"
-# connection_string = "mainnet-beta/"
-
-file_num = 1
+net = "devnet"
+connection_string = net + "_final_statistics.json"
 
 def get_df(timeFrame = "D"):
 
-    final_df = pd.DataFrame()
-    for file in natsorted(os.listdir(connection_string)):
-        df = pd.read_json(connection_string+file)
-        if df.empty:
-            continue
-        print(file)
+    df = pd.read_json(connection_string)
+    timestamp = df["date"]
 
-        timestamp = df["blockTime"]
-        
-        tstamp = pd.to_datetime(timestamp, unit='s')
-        # print("zero", tstamp)
-        tstamp = tstamp.dt.tz_localize('UTC')
-        # print("first", tstamp)
-        tstamp = tstamp.dt.tz_convert('US/Central')
-        # print("second", tstamp)
-        # print("\nTSTAMP\n",tstamp.dt.date, "\n\n")
-        df.index = tstamp
-        df['Amount'] = 1
+    tstamp = pd.to_datetime(timestamp)
 
-        # print("\nFINAL DF\n", df, "\n")
-        bucket_df = df.resample("D").Amount.sum().sort_index()
-
-        print("Bucket\n", bucket_df)
-        if not final_df.empty:
-            final_df = pd.concat([bucket_df, final_df])
-        else:
-            final_df = bucket_df
-
-    final_df = final_df.groupby(final_df.index).sum().sort_index()
-
-    return final_df
+    df.index = tstamp
+    df = df.resample(timeFrame).sum()
+    return df
 
 
 def plot_data():
     final_df = get_df("D")
     print("\n***\n", final_df)
-    print(f"num of transactions; {final_df.sum()}")
-    final_df.plot.line()
+    print(final_df.values)
+    
+    final_df.plot()
     plt.title('BPFLoader Interactions')
     plt.ylabel('Interaction Count')
     plt.xlabel('Dates')
